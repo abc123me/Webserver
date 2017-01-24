bd15fa2b367923628a3a3c449338ae0ee0ec6e0e
abc123mewot <abc123mewot@localhost>
2017-01-24 08:37:31 -0500

    Nothing

--- Website/index.php
+++ Website/index.php
@@ -2,12 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <body>
     <div id="main">
-    	<p>This is a php test</p>
-    	<?php
-
-        $cookie = http_build_cookie("test");
-
-    	?>
+    	<p><h1>Your bank.com</h1></p>
+    	<form>
+    		<p>Username: </p><input>
+    		<p>Password: </p><input type="Password">
+    	</form>
+    	<br>
+    	<button>Login</button>
     </div>
   </body>
 </html>
---
+++ Website/success.php
@@ -0,0 +1,6 @@
+<html>
+<head>Login success!</head>
+<body>
+	<p>You have logged in successfully!</p>
+</body>
+</html>
